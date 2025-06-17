6cd20eb8f1fb5fe18e84b7315a36dcd5d42df460
JSW <xquadoxx@gmail.com>
2025-06-18 01:53:43 +0900

    코리

--- applications/authentication/urls.py
+++ applications/authentication/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     ),
     path("join/", Join.as_view(), name="join"),
     path("logout/", Logout.as_view(), name="logout"),
-    
+    path("next-step/", NextStepView.as_view(), name="next-step"), 
 ]
